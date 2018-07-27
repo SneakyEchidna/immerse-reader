1e562d833177c4436af268b19d8fdf162e273cdb
Kanstantsin Kiyko <xfozzyx@gmail.com>
2018-07-27 19:18:28 +0300

    add parsed defininitions caching to firebase

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const { addWordtoStore, getWordFromStore } = require('./firebase');
 const { scrape, addWord } = require('./parser');
 
 const app = express();
@@ -7,8 +8,17 @@ scrape();
 
 const port = process.env.PORT || 5000;
 app.get('/api/:word/', (req, res) => {
-  const def = defs => res.send(defs);
-  addWord(req.params.word, def);
+  getWordFromStore(req.params.word).then((cachedDef) => {
+    if (cachedDef) {
+      res.send(cachedDef);
+    } else {
+      const def = (defs) => {
+        addWordtoStore(req.params.word, defs);
+        res.send(defs);
+      };
+      addWord(req.params.word, def);
+    }
+  });
 });
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
