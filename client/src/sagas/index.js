a6cdef71a96b76767882f6d9f7ca244c9d33a73f
Kanstantsin Kiyko <xfozzyx@gmail.com>
2018-07-27 12:18:17 +0300

    speed up def search with rxjs

--- client/src/sagas/index.js
+++ client/src/sagas/index.js
@@ -1,5 +1,5 @@
-import { takeEvery, delay } from 'redux-saga';
-import { fork, put, select, call } from 'redux-saga/effects';
+import { takeEvery } from 'redux-saga';
+import { fork, put, all } from 'redux-saga/effects';
 import { GET_DEFINITIONS } from '../actions/actionTypes';
 import { setDefinitions } from '../actions';
 
@@ -10,7 +10,7 @@ function* callGetDefinitions({ payload }) {
 
     if (response.status !== 200) throw Error(body.message);
 
-    return body[0];
+    return body;
   };
 
   const def = yield callApi(payload);
@@ -21,5 +21,5 @@ function* getdefinitionsSaga() {
   yield* takeEvery(GET_DEFINITIONS, callGetDefinitions);
 }
 export default function* rootSaga() {
-  yield [fork(getdefinitionsSaga)];
+  yield all([fork(getdefinitionsSaga)]);
 }
--- parser.js
+++ parser.js
@@ -1,25 +1,35 @@
 const puppeteer = require('puppeteer');
+const Rx = require('rxjs');
 
-const scrape = async (text) => {
+const pendingItems = new Rx.Subject();
+
+const addWord = (word, callback) => {
+  pendingItems.next({
+    word,
+    callback,
+  });
+};
+const scrape = async () => {
   const browser = await puppeteer.launch({
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    // slowMo: 10,
+    // slowMo: 100,
   });
   const url = 'https://en.oxforddictionaries.com/';
   const BUTTON_SELECTOR = 'div.searchMain > form > fieldset > button';
   const INPUT_SELECTOR = '#query';
-  const results = [];
   const page = await browser.newPage();
 
   page.on('console', e => console.log(e));
   await page.goto(url, { waitUntil: 'load' });
   console.log('page loaded');
-  results.push(await getDefinition(text));
-  console.log('def pushed');
-  browser.close();
-  console.log('browser closed');
-  return results;
+  pendingItems.subscribe(({ word, callback }) => {
+    getDefinition(word)
+      .then(result => callback(result))
+      .catch((reason) => {
+        console.error(reason);
+      });
+  });
 
   async function getDefinition(word) {
     await page.click(INPUT_SELECTOR);
@@ -44,4 +54,4 @@ const scrape = async (text) => {
     return result;
   }
 };
-module.exports = scrape;
+module.exports = { scrape, addWord };
