91bc175ec424ce1e65e465b70be4990cc454f242
Kanstantsin Kiyko <xfozzyx@gmail.com>
2018-09-07 11:00:17 +0300

    add redux persist store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,15 +1,24 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import createSagaMiddleware from 'redux-saga';
 import reducer from '../reducers';
 import rootSaga from '../sagas';
 
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 /* eslint-disable */
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(
-  reducer,
+export const store = createStore(
+  persistedReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(sagaMiddleware)
 );
+export const persistor = persistStore(store);
 sagaMiddleware.run(rootSaga);
 /* eslint-enable */
-export default store;
